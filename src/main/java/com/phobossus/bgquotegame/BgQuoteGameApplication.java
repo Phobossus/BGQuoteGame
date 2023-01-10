12fb25e4d9ce1433892eda98cb20a7c85151fc77
Aleksander <aleksander.gul113@gmail.com>
2023-01-10 19:00:34 +0100

    Project skeleton setup.

---
+++ src/main/java/com/phobossus/bgquotegame/BgQuoteGameApplication.java
@@ -0,0 +1,13 @@
+package com.phobossus.bgquotegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgQuoteGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BgQuoteGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/phobossus/bgquotegame/BgQuoteGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phobossus.bgquotegame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgQuoteGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
